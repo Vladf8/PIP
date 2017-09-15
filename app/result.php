a8bee493bdb6e790b35de850ac19f4099df983f0
Vladf8 <fishenko.vlad@mail.ru>
2017-09-15 18:21:09 +0300

    Add files via upload

---
+++ app/result.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Laboratory work 1</title>
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<h1>Лабораторная работа №1</h1>
+			<h3>Выполнили: Хлопков Д.С., Фищенко В. P3217</h3>
+			<h3>Проверил: Николаев В.В.</h3>
+		</header>
+		<div>
+			<?php
+				echo $_GET['y'];
+
+			?>
+		</div>
+	</body>
+</html>
