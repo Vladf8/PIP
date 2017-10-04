74a02d01a754b7bd179364b256452b4709313ff7
khlopkov <khlopkov@niuitmo.ru>
2017-10-04 15:29:36 +0300

    Add files via upload

--- app/result.php
+++ app/result.php
@@ -16,58 +16,86 @@ session_start();
 		</header>
 		<div>
 			<?php 
+				$time = microtime();
+				$validate = true;
+				$matches = '';
 				$x =  $_GET['x'];
 				$y =  $_GET['y'];
 				$r =  $_GET['r'];
+				$regInt = "/^[-?\d]+$/";
+				$regFloat = "/^[-?\d]+[.|,]?[\d]*$/";
+				if ((preg_match($regInt, $x, $matches) == 1) &&
+					(preg_match($regFloat, $y, $matches) == 1) &&
+					(preg_match($regInt, $r, $matches) == 1) &&
+					$x <= 4 && $x >= -4 &&
+					$y <= 3 && $y >= -3 &&
+					$r <= 5 && $r >= 1 )
+					$validate = true;
+				else
+					$validate = false;
 				$strike = true;
 			?>
 			<center>
 			<img src = "../pics/areas.png"><p>
-			<p>Координаты точки: (<?php echo $x;?> ; <?php echo $y;?>)
-			<p>Значение R: <?php echo $r;?> <br>
 			<?php
-				if ($x <= 0){
-					if ($y <= 0)
-						if ($y >= -$x - $r)
-							$strike = true;
-						else $strike = false;
+			    echo "<p>Координаты точки: (";
+			    $str=wordwrap($y, 3, "\n",1);
+			    echo $x;
+			    echo "; ";
+			    echo "$str";
+			    echo ")<p>Значение R: ";
+			    echo $r;
+			    echo "<br>";
+				if ($validate){
+					if ($x <= 0){
+						if ($y <= 0)
+							if ($y >= -$x - $r)
+								$strike = true;
+							else $strike = false;
+						else
+							if ($x*$x+$y*$y <= $r*$r)
+								$strike = true;
+							else $strike = false;
+					}
 					else
-						if ($x*$x+$y*$y <= $r*$r)
-							$strike = true;
+						if ($y >= 0)
+							if ($x<=$r && $y <= $r/2)
+								$strike = true;
+							else $strike = false;
 						else $strike = false;
+					if ($strike){
+						echo "Точка попадает в область.<br>";
+					}
+					else {
+						echo "Точка не попадает в область.<br>";
+					}
+					$time = microtime() - $time;
+					echo "Скрипт выполнялся ";
+					echo $time;
+					echo " секунд";
+					if (!isset($_SESSION['x'])) {
+						$_SESSION['x'] = array($x);
+					} else {
+						$_SESSION['x'][] = $x;
+					}
+					if (!isset($_SESSION['y'])) {
+						$_SESSION['y'] = array($y);
+					} else {
+						$_SESSION['y'][] = $y;
+					}
+					if (!isset($_SESSION['r'])) {
+						$_SESSION['r'] = array($r);
+					} else {
+						$_SESSION['r'][] = $r;
+					}
+					if (!isset($_SESSION['strike'])) {
+						$_SESSION['strike'] = array($strike);
+					} else {
+						$_SESSION['strike'][] = $strike;
+					}
 				}
 				else
-					if ($y >= 0)
-						if ($x<=$r && $y <= $r/2)
-							$strike = true;
-						else $strike = false;
-					else $strike = false;
-				if ($strike){
-					echo "Точка попадает в область.<br>";
-				}
-				else {
-					echo "Точка не попадает в область.<br>";
-				}
-				if (!isset($_SESSION['x'])) {
-					$_SESSION['x'] = array($x);
-				} else {
-					$_SESSION['x'][] = $x;
-				}
-				if (!isset($_SESSION['y'])) {
-					$_SESSION['y'] = array($y);
-				} else {
-					$_SESSION['y'][] = $y;
-				}
-				if (!isset($_SESSION['r'])) {
-					$_SESSION['r'] = array($r);
-				} else {
-					$_SESSION['r'][] = $r;
-				}
-				if (!isset($_SESSION['strike'])) {
-					$_SESSION['strike'] = array($strike);
-				} else {
-					$_SESSION['strike'][] = $strike;
-				}
+					echo "Данные введены неверно";
 			?>
 			<table border = "1">
 				<caption>Таблица всех значений и их попаданий</caption>
@@ -84,7 +112,8 @@ session_start();
 						echo $_SESSION['x'][$i];
 						echo "</td>";
 						echo "<td>";
-						echo $_SESSION['y'][$i];
+						$str=wordwrap($_SESSION['y'][$i], 3,"\n",1) ;
+						echo "$str";
 						echo "</td>";
 						echo "<td>";
 						echo $_SESSION['r'][$i];
@@ -98,7 +127,7 @@ session_start();
 					}
 				?>
 			</table>
-			<a href="https://se.ifmo.ru/~s223375/" style="color: red">Назад</a>
+			<a href="../index.html" style="color: red">Назад</a>
 		</center>
 		</div>
 	</body>
